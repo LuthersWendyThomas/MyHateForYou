3d00a4e3e72b5b99d7b82c7a2d99ec7f5123eea7
LuthersWendyThomas <210305113+LuthersWendyThomas@users.noreply.github.com>
2025-05-06 13:45:12 +0300

    Update finalHandler.js

--- core/handlers/finalHandler.js
+++ core/handlers/finalHandler.js
@@ -133,12 +133,12 @@ function greetingText(count) {
 🚚 Delivery in *30 minutes or less*  
 🕵️ Ultra-discreet • No questions asked  
 
-✨ Trusted Quality Since 2020 🔥  
-✨ 24/7 Live Support & Fully Automated Service 🤖  
-✨ Drop / Courier Options Available* 🚚  
+✨ Trusted Quality Since 2020
+✨ 24/7 Live Support & Fully Automated Service
+✨ *Drop / Courier Options Available*
 
-📍 *Drop anywhere in your city* 🌆  
-📍 *Courier to your agreed location* 🚚  
+🌆 *Drop anywhere in your city* 
+🚚 *Courier to your agreed location*  
 
 ❗️ *Do not speak or photograph couriers*  
 ⛔ Any violation = instant *BAN*  
